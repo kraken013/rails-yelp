c512c924a8a88b5fad1eec2cb8c5da73d3d820ac
sofiane flici <sofiane.flici14@gmail.com>
2023-06-14 14:12:14 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants_attributes = [
+  {
+    name: "Dishoom",
+    address: "7 Boundary St, London E2 7JE",
+    phone_number: "01 23 45 67 89",
+    category: "chinese",
+  },
+  {
+    name: "Pizza East",
+    address: "56A Shoreditch High St, London E1 6PQ",
+    phone_number: "01 23 45 67 89",
+    category: "italian",
+  },
+  {
+    name: "Pizza West",
+    address: "56A Shoreditch High St, London E1 6PQ",
+    phone_number: "01 23 45 67 89",
+    category: "italian",
+  },
+  {
+    name: "Pizza North",
+    address: "56A Shoreditch High St, London E1 6PQ",
+    phone_number: "01 23 45 67 89",
+    category: "italian",
+  },
+  {
+    name: "Pizza South",
+    address: "56A Shoreditch High St, London E1 6PQ",
+    phone_number: "01 23 45 67 89",
+    category: "italian",
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts "Finished!"
